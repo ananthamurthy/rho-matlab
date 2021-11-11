4d286f93767e90701aed39a052b7c5fbbeccb197
ananthamurthy <kganantha@gmail.com>
2021-11-11 11:01:52 +0530

    now printing iSet for runtimes

--- src/paperFigures.m
+++ src/paperFigures.m
@@ -576,7 +576,7 @@ if profilerTest
             elseif myMethod == 7
                 elapsedTime = runBatchAnalysis(1, 3, 0, 0, 0, 0, 0, 1, gDate2, gRun2, workingOnServer, diaryOn, profilerTest);
             end
-            sprintf('Analysis of %i datasets by %s took %d mins.\n', num2str(3), char(methodLabels(myMethod)), elapsedTime/60)
+            sprintf('Set %i: Analysis of %i datasets by %s took %d mins.\n', iSet, num2str(3), char(methodLabels(myMethod)), elapsedTime/60)
             
             runTime(iSet, myMethod) = elapsedTime/60;
         end
