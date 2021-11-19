8bacbf8aaf578200a915bdc9bd15efbe1472e193
ananthamurthy <kganantha@gmail.com>
2021-11-19 14:12:46 +0530

    setupSyntheticdataParametersSingle.m now has the standard config

--- src/doRuntimeTest.m
+++ src/doRuntimeTest.m
@@ -1,4 +1,4 @@
-function runTime = profilerTest(gDate, nSets, nMethods, workingOnServer, diaryOn)
+function runTime = doRuntimeTest(gDate, nSets, nProcedures, workingOnServer, diaryOn)
 
 if workingOnServer == 1
     HOME_DIR = '/home/bhalla/ananthamurthy/';
@@ -17,32 +17,30 @@ procedureLabels = {'Synthesis', 'R2B (A)', 'TI (B)', 'Peak AUC/Std (C)', 'PCA (D
 myProfilerTest = 1;
 if myProfilerTest
     % Runtime and profile - Synthesis % Analysis
-    runTime = zeros(nSets, nMethods+1);
+    runTime = zeros(nSets, nProcedures);
     for iSet = 1:nSets
         gRun = iSet; %For example datasets - Profiler On
         
-        [~, elapsedTime] = generateSyntheticData(gDate, gRun, workingOnServer, diaryOn, myProfilerTest);
-        
-        for myMethod = 1:nMethods+1
-            if myMethod == 1
+        for myProcedure = 1:nProcedures
+            if myProcedure == 1
                 %use the elapsedTime for generation
-                %Offset by 1
-            elseif myMethod == 2
+                [~, elapsedTime] = generateSyntheticData(gDate, gRun, workingOnServer, diaryOn, myProfilerTest);
+            elseif myProcedure == 2
                 elapsedTime = runBatchAnalysis(1, 3, 1, 0, 0, 0, 0, 0, gDate, gRun, workingOnServer, diaryOn, myProfilerTest);
-            elseif myMethod == 3
+            elseif myProcedure == 3
                 elapsedTime = runBatchAnalysis(1, 3, 0, 1, 0, 0, 0, 0, gDate, gRun, workingOnServer, diaryOn, myProfilerTest);
-            elseif myMethod == 4
+            elseif myProcedure == 4
                 elapsedTime = runBatchAnalysis(1, 3, 0, 0, 1, 0, 0, 0, gDate, gRun, workingOnServer, diaryOn, myProfilerTest);
-            elseif myMethod == 5
+            elseif myProcedure == 5
                 elapsedTime = runBatchAnalysis(1, 3, 0, 0, 0, 1, 0, 0, gDate, gRun, workingOnServer, diaryOn, myProfilerTest);
-            elseif myMethod == 6
+            elseif myProcedure == 6
                 elapsedTime = runBatchAnalysis(1, 3, 0, 0, 0, 0, 1, 0, gDate, gRun, workingOnServer, diaryOn, myProfilerTest);
-            elseif myMethod == 7
+            elseif myProcedure == 7
                 elapsedTime = runBatchAnalysis(1, 3, 0, 0, 0, 0, 0, 1, gDate, gRun, workingOnServer, diaryOn, myProfilerTest);
             end
-            sprintf('Set: %i -> nDatasets: 3 -> Procedure: %s -> Time: %d mins.\n', iSet, char(procedureLabels(myMethod)), elapsedTime/60)
+            sprintf('Set: %i -> nDatasets: 3 -> Procedure: %s -> Time: %d mins.\n', iSet, char(procedureLabels(myProcedure)), elapsedTime/60)
             
-            runTime(iSet, myMethod) = elapsedTime/60;
+            runTime(iSet, myProcedure) = elapsedTime/60;
         end
     end
     save([HOME_DIR 'rho-matlab/runtimes.mat'])
