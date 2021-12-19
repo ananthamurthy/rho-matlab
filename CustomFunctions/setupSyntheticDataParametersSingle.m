dacf2ac912e4a31f0bb68681401d0c1390b5fc72
ananthamurthy <kganantha@gmail.com>
2021-12-19 10:03:54 +0530

    Have a working experiment for memory and runtime now. got results for nShuffles = 1 and 4.

--- CustomFunctions/setupSyntheticDataParametersSingle.m
+++ CustomFunctions/setupSyntheticDataParametersSingle.m
@@ -25,7 +25,7 @@ rng('seed', 'generator')  %See help rng for details; Typically: 'default' or 'sh
 %%
 i = 0;
 %% Synthetic Data Parameters
-nShuffles = 3;
+nShuffles = 1;
 for shuffle = 1:nShuffles
     i = i + 1;
     sdcp(i).timeCellPercent = 50;
