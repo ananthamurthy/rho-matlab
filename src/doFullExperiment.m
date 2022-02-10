76b7f3ee7d17f9fc779a40c30d0e0f68c7015848
ananthamurthy <kganantha@gmail.com>
2022-02-10 10:30:34 +0530

    now going for a full test run.

--- src/doFullExperiment.m
+++ src/doFullExperiment.m
@@ -36,8 +36,8 @@ for myProcedure = 1:nProcedures
         elseif myProcedure == 7
             [~, totalMem, elapsedTime] = runBatchAnalysis(1, nTotalDatasets, 0, 0, 0, 0, 0, 1, gDate, gRun, workingOnServer, diaryOn, myProfilerTest);
         end
-        sprintf('Set: %i, nDatasets: %i, Procedure: %s, Time: %d mins., Mem.: %.4f MB\n', iSet, nTotalDatasets, char(procedureLabels(myProcedure)), elapsedTime/60, totalMem)
+        sprintf('Procedure: %s, nDatasets: %i, Time: %d sec, Mem.: %.4f MB\n', char(procedureLabels(myProcedure)), nTotalDatasets, elapsedTime, totalMem)
 
-        runTime(myProcedure) = elapsedTime/60;
-        inUse(myProcedure) = totalMem; %as Bytes
+        runTime(myProcedure) = elapsedTime; %in secs
+        inUse(myProcedure) = totalMem; %in Bytes
 end
