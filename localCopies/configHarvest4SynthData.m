e00e48fa16c979610a6fb067ab2bdd71e06e6b3a
ananthamurthy <kganantha@gmail.com>
2022-03-11 11:08:24 +0530

    some more renaming for clarity on synthetic vs real data

---
+++ localCopies/configHarvest4SynthData.m
@@ -0,0 +1,9 @@
+disp('Setting up parameters for harvest ...')
+
+%setupHarvestParamsforSynthData %Loads all options for Physiology
+%setupHarvestParamsforSynthData2 %Loads all options for PSA
+%setupHarvestParamsforSynthData3
+%setupHarvestParamsforSynthData4
+%setupHarvestParamsforSynthData5
+setupHarvestParamsforSynthData6
+disp('... done!')
