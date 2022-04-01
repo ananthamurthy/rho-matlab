de3822f6dbffa4240cfcc682fd01488811fbced8
ananthamurthy <kganantha@gmail.com>
2022-04-01 12:03:22 +0530

    trying the real dataset batch again, just to be sure

--- src/plotRealDataAnalysisResults.m
+++ src/plotRealDataAnalysisResults.m
@@ -50,7 +50,7 @@ figureDetails = compileFigureDetails(12, 2, 5, 0.2, 'inferno'); %(fontSize, line
 methodLabels = {'rR2B', 'Ispk', 'Isec', 'MI', 'pAUC', 'offPCA', 'Param.'};
 algoLabels = {'rR2B-Bo', 'rR2B-Ot', 'Ispk-Bo', 'Isec-Bo', 'MI-Bo', 'Ispk-Ot', 'Isec-Ot', 'MI-Ot', 'pAUC-Bo', 'pAUC-Ot', 'offPCA-Ot', 'Param-Bo', 'Param-Ot'};
 metricLabels = {'Recall', 'Precision', 'F1 Score'};
-concordanceAlgoLabels = {'>=1', '>=2', '>=3', '>= 4', '>= 5', '>= 6', '>=7', '>=8', '>=9', '>=10', '>=11', '>=12', '>=13', '=14'};
+concordanceAlgoLabels = {'>=1', '>=2', '>=3', '>= 4', '>= 5', '>= 6', '>=7', '>=8', '>=9', '>=10', '>=11', '>=12', '=13'};
 
 allPredictors = [];
 allPredictions = [];
@@ -126,18 +126,39 @@ for dnum = 1:input.nDatasets
     %         '-djpeg')
 
     fig3 = figure(3);
+    set(fig3, 'Position', [100, 100, 1200, 600])
     clf
+    subplot(2, 3, (1:3))
     plotdFbyF(db0(dnum), DATA_2D, 'Real', 'All Frames', 'All Cells', figureDetails, 1)
     set(gca, 'FontSize', figureDetails.fontSize)
 
+    subplot(2, 3, (4:6))
+    imagesc(squeeze(mean(DATA, 2))*100)
+    title('Trial Avg.', ...
+        'FontSize', figureDetails.fontSize, ...
+        'FontWeight','bold')
+    xlabel('Frames', ...
+        'FontSize', figureDetails.fontSize, ...
+        'FontWeight','bold')
+%     ylabel('Cells', ...
+%         'FontSize', figureDetails.fontSize, ...
+%         'FontWeight','bold')
+    z = colorbar;
+    ylabel(z, 'dF/F (%)')
+    set(gca, 'FontSize', figureDetails.fontSize)
+    
+    print(sprintf('%s%s_%s_visualization', HOME_DIR2, db0(dnum).mouseName, db0(dnum).date), ...
+        '-djpeg')
 end
 fprintf('Total Cells Analyzed: %i\n', nCellsAnalyzed)
 
 percentTCpredictions = (sum(allPredictions, 1)/nCellsAnalyzed)*100;
 fig4 = figure(4);
+clf
 set(fig4, 'Position', [100, 100, 1200, 600])
+subplot(2, 1, 1)
 bar(percentTCpredictions)
-title('How many Time Cells?', ...
+title(sprintf('How many Time Cells? Total Cells: %i\n', nCellsAnalyzed), ...
     'FontSize', figureDetails.fontSize, ...
     'FontWeight','bold')
 xlabel('Algorithms', ...
@@ -148,10 +169,6 @@ xticklabels(algoLabels)
 ylabel('Predictions (%)', ...
     'FontSize', figureDetails.fontSize, ...
     'FontWeight','bold')
-print(sprintf('%sallPredictions', HOME_DIR2), ...
-    '-djpeg')
-elapsedTime3 = toc;
-fprintf('Elapsed Time: %.4f seconds\n', elapsedTime3)
 
 % Concordance - slightly different algorithm from just using compareAgainstTruth()
 input.removeNaNs = 1;
@@ -162,120 +179,26 @@ nCases = length(Y);
 % Now, for any cell, if sumX >= some threshold, we suggest calling it a Time Cell
 nConcordanceAlgos = 14; % since input.nAlgos = 8
 
-predictions = zeros(nCases, nConcordanceAlgos);
-
-%Preallocation
-allTP = zeros(nCases, nConcordanceAlgos);
-allTN = zeros(nCases, nConcordanceAlgos);
-allFP = zeros(nCases, nConcordanceAlgos);
-allFN = zeros(nCases, nConcordanceAlgos);
-TPR = zeros(nConcordanceAlgos, 1); %Sensitivity, Recall, or True Positive Rate
-FPR = zeros(nConcordanceAlgos, 1); %Fall-out or False Positive Rate
-TNR = zeros(nConcordanceAlgos, 1); %Specificity or True Negative Rate
-FNR = zeros(nConcordanceAlgos, 1); %Miss Rate or False Negative Rate
-PPV = zeros(input.nAlgos, 1); %Precision or Positive Predictive Value
-
-results4 = zeros(nConcordanceAlgos, 3); %3 because we'll look at Recall, Precision, and F1 Score
-results5 = zeros(nConcordanceAlgos, 2); %TP and FP
+concordancePredictions = zeros(nCases, nConcordanceAlgos);
 
 for algo = 1:nConcordanceAlgos
-
-    predictions(:, algo) = (sumX >= algo);
-
-    for myCase = 1:nCases
-        if Y(myCase) %Positive Cases
-            if predictions(myCase, algo)
-                allTP(myCase, algo) = 1;
-            else
-                allFN(myCase, algo) = 1;
-            end
-        else %Negative Cases
-            if predictions(myCase, algo)
-                allFP(myCase, algo) = 1;
-            else
-                allTN(myCase, algo) = 1;
-            end
-        end
-    end
-
-    TP = sum(allTP(:, algo));
-    FN = sum(allFN(:, algo));
-    FP = sum(allFP(:, algo));
-    TN = sum(allTN(:, algo));
-
-    if TP ~= 0
-        TPR(algo) = TP/(TP + FN); %Recall
-        PPV(algo) = TP/(TP + FP); %Precision
-    else
-        TPR(algo) = 0; %Recall
-        PPV(algo) = 0; %Precision
-    end
-
-    if FP ~= 0
-        FPR(algo) = FP/(FP + TN);
-    else
-        FPR(algo) = 0;
-    end
-
-    if TN ~= 0
-        TNR(algo) = TN/(TN + FP);
-    else
-        TNR(algo) = 0;
-    end
-
-    if FN ~= 0
-        FNR(algo) = FN/(FN + TP);
-    else
-        FNR(algo) = 0;
-    end
-
-    results4(algo, 1) = TPR(algo); %Recall
-    results4(algo, 2) = PPV(algo); %Precision
-
-    if TP == 0
-        results4(algo, 3) = 0;
-    else
-        results4(algo, 3) = 2 * results4(algo, 1) * results4(algo, 2)/(results4(algo, 1) + results4(algo, 2)); %F1 Score
-    end
-
-    results5(algo, 1) = TP;
-    results5(algo, 2) = FP;
+    concordancePredictions(:, algo) = (sumX >= algo);
 end
 
-fig5 = figure(5);
-clf
-subplot(2, 1, 1)
-d1 = bar(results4);
-xlim([0 5])
-axis tight
-xlabel('Classification Concordance Threshold', ...
-    'FontSize', figureDetails.fontSize, ...
-    'FontWeight', 'bold')
-ylabel('Rate', ...
-    'FontSize', figureDetails.fontSize, ...
-    'FontWeight', 'bold')
-xticklabels(concordanceAlgoLabels)
-xtickangle(45)
-lgd = legend(metricLabels, ...
-    'Location', 'southeastoutside');
-lgd.FontSize = figureDetails.fontSize-3;
-set(gca, 'FontSize', figureDetails.fontSize)
-
 subplot(2, 1, 2)
-d2 = bar(results5);
-title(sprintf('Num. Total Cells: %i', nCellsAnalyzed), ...
-    'FontSize', figureDetails.fontSize, ...
-    'FontWeight', 'bold')
+d = bar((sum(concordancePredictions, 1)*100)/size(concordancePredictions, 1));
 xlabel('Concordance Threshold', ...
     'FontSize', figureDetails.fontSize, ...
-    'FontWeight', 'bold')
-xticklabels(concordanceAlgoLabels);
+    'FontWeight','bold')
+xticks(1:input.nAlgos)
+xticklabels(concordanceAlgoLabels)
 ylabel('Predictions (%)', ...
     'FontSize', figureDetails.fontSize, ...
-    'FontWeight', 'bold')
-set(gca, 'FontSize', figureDetails.fontSize)
+    'FontWeight','bold')
 
 print(sprintf('%snPredictions', HOME_DIR2), ...
     '-djpeg')
 
+elapsedTime3 = toc;
+fprintf('Elapsed Time: %.4f seconds\n', elapsedTime3)
 disp('... All Done!')
