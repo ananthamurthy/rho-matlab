f5212996d65b0e461e80c4e93cd57afeab420fa7
ananthamurthy <ananthamurthy@ncbs.res.in>
2022-08-11 19:51:01 +0530

    small update

--- CustomFunctions/compileFigureDetails.m
+++ CustomFunctions/compileFigureDetails.m
@@ -8,21 +8,23 @@ figureDetails.markerSize     = markerSize;
 figureDetails.transparency   = transparency;
 
 if strcmpi(colorMap, 'magma')
-    %magma=magma();
     figureDetails.colorMap = magma();
 elseif strcmpi(colorMap, 'inferno')
-    %inferno=inferno();
     figureDetails.colorMap = inferno();
 elseif strcmpi(colorMap, 'plasma')
-    %plasma=plasma();
     figureDetails.colorMap = plasma();
 elseif strcmpi(colorMap, 'viridis')
-    %viridis=viridis();
     figureDetails.colorMap = viridis();
 elseif strcmpi(colorMap, 'hot')
     figureDetails.colorMap = hot();
 elseif strcmpi(colorMap, 'jet')
     figureDetails.colorMap = jet();
+elseif strcmpi(colorMap, 'pmkmp')
+    figureDetails.colorMap = pmkmp(256);
+elseif strcmpi(colorMap, 'cividis')
+    figureDetails.colorMap = cividis();
+elseif strcmpi(colorMap, 'linspecer')
+    figureDetails.colorMap = linspecer();
 else
     figureDetails.colorMap    = colorMap;
 end
